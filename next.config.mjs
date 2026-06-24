042d95c4a5fc6a63c97d4532318ae2eceb0165a3
hasantopcuu5-byte <hasantopcuu5@gmail.com>
2026-06-24 16:45:23 +0300

    cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // IP üzerinden dev erişiminde HMR WebSocket'ine izin ver (örn. 10.30.0.156:3000)
+  // Cloudflare'e projenin statik dosyalar olarak derlenmesini söyler
+  output: 'export', 
+  
   allowedDevOrigins: ["10.30.0.156", "localhost"],
   typescript: {
     ignoreBuildErrors: true,
